c641b58c4a2a689a21275ce5a5c18cfc12f951cd
Federico Miras <miras.federico@gmail.com>
2023-09-17 18:34:13 -0300

    feat: cargo init

---
+++ examples/simmetric.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Encrypting using simmetric key");
+
+    let target_string = "fefowashere";
+    println!("🎯 Target string: {}", target_string);
+
+    println!("Generating key...");
+    let key = "some-key";
+    println!("🔑 Key: {}", key);
+}
